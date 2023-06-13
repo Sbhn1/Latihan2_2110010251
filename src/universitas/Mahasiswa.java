70af87fb20dedfb0fc933cdef3a366056b6a813e
Sbhn1 <subhansyn@gmail.com>
2023-06-13 19:01:44 +0700

    Melengkapi class Mahasiswa

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universitas;
 
-/**
- *
- * @author hp
- */
+
 public class Mahasiswa {
+    //Membuat variabel
+    private String npm,nama,prodi;
     
+    //Membuat method contructor
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+   
 }
